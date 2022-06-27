419f14324e01dee40f00394cbf090b3406a95abc
annisaqadn <annisaqadn@gmail.com>
2022-06-27 18:03:59 +0700

    Sayur Box

---
+++ script.js
@@ -0,0 +1,168 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-bar').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 100){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').classList.add('active');
+    }else{
+        document.querySelector('body').classList.remove('active');
+    }
+}
+
+document.querySelectorAll('.small-image-1').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-1').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-2').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-2').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-3').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-3').src = images.getAttribute('src');
+    }
+});
+
+let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+
+function countDown(){
+
+    let now = new Date().getTime();
+	gap = countDate - now;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+
+    let d = Math.floor(gap / (days));
+	let h = Math.floor((gap % (days)) / (hours));
+	let m = Math.floor((gap % (hours)) / (minutes));
+	let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000);
+
+var swiper = new Swiper(".product-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+function tMessage() {
+    var time=setTimeout("alert('Pesanan anda sudah masuk dan kami akan segera proses pesanan anda')", 2000);
+}
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name : ","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
